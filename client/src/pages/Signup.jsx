1bc4fc795b1f85659f5af821b2cf96786c0869a9
leanghok120 <leanghokoeng5@gmail.com>
2024-11-05 04:51:55 +0700

    Finish Signup page design

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,5 +1,31 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 export default function Signup() {
-  return <div>Signup</div>;
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <div className="flex flex-col justify-center items-center h-full">
+      <Navbar />
+      <h1 className="text-5xl font-black">Signup</h1>
+      <form
+        className="mt-10 flex flex-col gap-2 bg-base-200 p-10 rounded-2xl"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          placeholder="Username"
+          className="input input-bordered w-full max-w-xs"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="input input-bordered w-full max-w-xs"
+        />
+        <button className="btn btn-primary">Signup</button>
+      </form>
+    </div>
+  );
 }
