390a80af0edbf505de19293ebc59b0583ca65254
leanghok120 <leanghokoeng5@gmail.com>
2024-11-05 05:21:38 +0700

    Add routes to navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,29 +1,33 @@
 import React from "react";
 import { Home, Plus, User } from "lucide-react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <div className="fixed top-0 left-0 h-screen w-16 p-4">
       <div className="flex flex-col justify-between h-full w-full items-center">
         <div className="flex flex-col space-y-4 items-center">
-          <button className="hover:scale-125 active:scale-150 transition-all w-12">
+          <Link
+            className="hover:scale-125 active:scale-150 transition-all w-12"
+            to="/"
+          >
             <img src="/logo.svg" alt="logo" />
-          </button>
+          </Link>
         </div>
 
         <div className="flex flex-col space-y-4 items-center">
-          <button className="btn btn-ghost">
+          <Link className="btn btn-ghost" to="/">
             <Home className="w-6 h-6" />
-          </button>
-          <button className="btn btn-ghost">
+          </Link>
+          <Link className="btn btn-ghost" to="/create">
             <Plus className="w-6 h-6" />
-          </button>
+          </Link>
         </div>
 
         <div className="flex flex-col space-y-4 items-center">
-          <button className="btn btn-ghost">
+          <Link className="btn btn-ghost" to="/signup">
             <User className="w-6 h-6" />
-          </button>
+          </Link>
         </div>
       </div>
     </div>
