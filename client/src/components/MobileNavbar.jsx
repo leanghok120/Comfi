0a1dc613d4a231fd8209bba65e07406ec088318b
leanghok120 <leanghokoeng5@gmail.com>
2024-11-10 10:08:53 +0700

    Fix mobile navbar

--- client/src/components/MobileNavbar.jsx
+++ client/src/components/MobileNavbar.jsx
@@ -5,7 +5,7 @@ import ProfileBtn from "./ProfileBtn";
 
 export default function MobileNavbar() {
   return (
-    <div className="fixed bottom-0 left-0 w-full p-2 bg-base-200 flex justify-around items-center">
+    <div className="fixed bottom-0 left-0 w-full p-2 bg-base-200 flex justify-around items-center z-50">
       <Link className="btn btn-ghost" to="/">
         <Home className="w-6 h-6" />
       </Link>
