853d5bab655d87a451a19379cc534f8666825ab3
leanghok <leanghokoeng5@gmail.com>
2025-02-11 05:05:02 +0700

    add the frontend for the signin page

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,4 +1,5 @@
+import { env } from '$env/dynamic/private';
 import { createAuthClient } from 'better-auth/svelte';
 export const authClient = createAuthClient({
-	baseURL: 'http://localhost:5173'
+	baseURL: env.BETTER_AUTH_URL
 });
--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -1,1 +1,15 @@
-<h1>signin page</h1>
+<form class="flex min-h-screen items-center justify-center">
+	<fieldset class="fieldset bg-base-100 border-base-300 rounded-box w-96 border p-4 shadow-sm">
+		<legend class="fieldset-legend">Login</legend>
+
+		<label class="fieldset-label">Email</label>
+		<input type="email" class="input w-full" placeholder="Email" />
+
+		<label class="fieldset-label">Password</label>
+		<input type="password" class="input w-full" placeholder="Password" />
+
+		<button class="btn btn-primary mt-4 w-full">Login</button>
+
+		<p class="text-error">doesn't work yet!</p>
+	</fieldset>
+</form>
