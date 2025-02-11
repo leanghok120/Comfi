5730a8d28116a147e2687b1a4f5b62997fb39471
leanghok <leanghokoeng5@gmail.com>
2025-02-11 19:26:00 +0700

    remove email password login (for now)

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -6,15 +6,6 @@
 	<fieldset class="fieldset bg-base-100 border-base-300 rounded-box w-96 border p-4 shadow-sm">
 		<legend class="fieldset-legend">Login</legend>
 
-		<!-- svelte-ignore a11y_label_has_associated_control -->
-		<label class="fieldset-label">Email</label>
-		<input type="email" class="input w-full" placeholder="Email" />
-
-		<!-- svelte-ignore a11y_label_has_associated_control -->
-		<label class="fieldset-label">Password</label>
-		<input type="password" class="input w-full" placeholder="Password" />
-
-		<button class="btn btn-primary mt-4 w-full">Login</button>
 		<button
 			class="btn mt-4"
 			onclick={async () => {
